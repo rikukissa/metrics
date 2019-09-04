7e794b6ba026a866b7037edda1543f0b9cd8bf84
Riku Rouvila <riku.rouvila@gmail.com>
2019-09-04 08:22:17 +0300

    add javascript entrypoint

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-require("ts-node").register();
+#!/usr/bin/env node
 
+require("ts-node").register({
+  compilerOptions: require("./tsconfig.json").compilerOptions
+});
 require("./src/index.ts").run();
